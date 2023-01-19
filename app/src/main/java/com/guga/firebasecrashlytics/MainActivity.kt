9653d4b0945d1c20e19949968295fa5a7ec63f7b
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-01-19 16:22:37 -0300

    starting to manipulate xml to simulate a error using crashlytics

--- app/src/main/java/com/guga/firebasecrashlytics/MainActivity.kt
+++ app/src/main/java/com/guga/firebasecrashlytics/MainActivity.kt
@@ -2,10 +2,20 @@ package com.guga.firebasecrashlytics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.guga.firebasecrashlytics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnButton.setOnClickListener {
+
+            //could simulate a error when user click on button
+
+        }
     }
 }
